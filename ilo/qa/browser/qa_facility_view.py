98ac102869d5418ac4187b8497d19071d8c39e45
sescatalan <sandrea@afterfivetech.com>
2015-06-17 11:38:30 +0800

    modified qa facility browser view

--- ilo/qa/browser/qa_facility_view.py
+++ ilo/qa/browser/qa_facility_view.py
@@ -1,6 +1,7 @@
 from five import grok
 from plone.directives import dexterity, form
 from ilo.qa.content.qa_facility import IQAFacility
+from Products.CMFCore.utils import getToolByName
 
 grok.templatedir('templates')
 
@@ -10,3 +11,13 @@ class Index(dexterity.DisplayForm):
     grok.template('qa_facility_view')
     grok.name('view')
 
+    @property
+    def catalog(self):
+    	return getToolByName(self.context, 'portal_catalog')
+
+    def contents(self):
+    	context = self.context
+    	catalog = self.catalog
+    	path = '/'.join(context.getPhysicalPath())
+    	brains = catalog.unrestrictedSearchResults(path={'query': path, 'depth' : 1}, portal_type='ilo.qa.question',review_state='published',sort_on='Date',sort_order='reverse')[:11]
+    	return brains
