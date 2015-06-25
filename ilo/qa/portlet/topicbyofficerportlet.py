a8ef3465345ca4ba617b745ee14c7b1c0c5cefff
Kaka <sandrea@afterfivetech.com>
2015-06-25 16:38:12 +0800

    modified portlet: removed dropdown

--- ilo/qa/portlet/topicbyofficerportlet.py
+++ ilo/qa/portlet/topicbyofficerportlet.py
@@ -52,55 +52,87 @@ class Renderer(base.Renderer):
     def catalog(self):
         return getToolByName(self.context, 'portal_catalog')
 
-    def officers(self):
+    def officers_contents(self):
         context = self.context
         catalog = self.catalog
         path = '/'.join(context.getPhysicalPath())
-        results = [{'name':''}]
+        results = []
         brains = catalog.unrestrictedSearchResults(path={'query': path, 'depth' : 1}, portal_type='ilo.qa.topic',review_state='internally_published',sort_on='Date',sort_order='reverse')
         for brain in brains:
             obj = brain._unrestrictedGetObject()
             if not any(d['name'].lower() == obj.officer.lower() for d in results):
-                results.append({'name':obj.officer.lower()})
+                results.append({'name':obj.officer,
+                                'data': self.contents1(obj.officer.lower())['topics']})
         return results
 
-    def searchedValue(self, name=None):
-        result = ''
-        if self.request.form:
-            form = self.request.form
-            result = form[name]
-        return result
-
-
-    def contents1(self):
+    def contents1(self, officer=None):
         context = self.context
         request = self.request
         form = request.form
         catalog = self.catalog
-        officer='None'
         results = []
         path = '/'.join(context.getPhysicalPath())
         brains = catalog.unrestrictedSearchResults(path={'query': path, 'depth' : 1}, portal_type='ilo.qa.topic',review_state='internally_published',sort_on='Date',sort_order='reverse')
-        if form:
-            officer = form['topicbyofficer']
         i = 0
         for brain in brains:
             obj = brain._unrestrictedGetObject()
             if obj.officer.lower() in officer :
                 i = i + 1
                 results.append({'title': brain.Title,
-                                'id': brain.getId,
-                                'path':brain.getPath()})
+                                'id': brain.getId,})
                 if i == 10:
                     break;
-            if officer == 'all':
-                i = i + 1
-                results.append({'title': brain.Title,
-                                'id': brain.getId,
-                                'path':brain.getPath()})
-                if i == 10:
-                    break;
-        return (results, officer, request)
+        return {'topics': results}
+
+    # def officers(self):
+    #     context = self.context
+    #     catalog = self.catalog
+    #     path = '/'.join(context.getPhysicalPath())
+    #     results = [{'name':''}]
+    #     brains = catalog.unrestrictedSearchResults(path={'query': path, 'depth' : 1}, portal_type='ilo.qa.topic',review_state='internally_published',sort_on='Date',sort_order='reverse')
+    #     for brain in brains:
+    #         obj = brain._unrestrictedGetObject()
+    #         if not any(d['name'].lower() == obj.officer.lower() for d in results):
+    #             results.append({'name':obj.officer.lower()})
+    #     return results
+
+    # def searchedValue(self, name=None):
+    #     result = ''
+    #     if self.request.form:
+    #         form = self.request.form
+    #         result = form[name]
+    #     return result
+
+
+    # def contents1(self):
+    #     context = self.context
+    #     request = self.request
+    #     form = request.form
+    #     catalog = self.catalog
+    #     officer='None'
+    #     results = []
+    #     path = '/'.join(context.getPhysicalPath())
+    #     brains = catalog.unrestrictedSearchResults(path={'query': path, 'depth' : 1}, portal_type='ilo.qa.topic',review_state='internally_published',sort_on='Date',sort_order='reverse')
+    #     if form:
+    #         officer = form['topicbyofficer']
+    #     i = 0
+    #     for brain in brains:
+    #         obj = brain._unrestrictedGetObject()
+    #         if obj.officer.lower() in officer :
+    #             i = i + 1
+    #             results.append({'title': brain.Title,
+    #                             'id': brain.getId,
+    #                             'path':brain.getPath()})
+    #             if i == 10:
+    #                 break;
+    #         if officer == 'all':
+    #             i = i + 1
+    #             results.append({'title': brain.Title,
+    #                             'id': brain.getId,
+    #                             'path':brain.getPath()})
+    #             if i == 10:
+    #                 break;
+    #     return (results, officer, request)
 
 class AddForm(base.AddForm):
     form_fields = form.Fields(IContentNavigation)
